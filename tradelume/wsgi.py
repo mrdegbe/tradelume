8e1d9abfc342aa1e16f6e1851748e77124b86e66
Raymond Degbe <198380402+mrdegbe@users.noreply.github.com>
2025-05-31 07:58:57 +0100

    refactor(core): rename project to tradelume

--- tradelume/wsgi.py
+++ tradelume/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for tradejournal project.
+WSGI config for tradelume project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tradejournal.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tradelume.settings")
 
 application = get_wsgi_application()
